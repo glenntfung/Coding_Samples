9f1bded68ee8069eb26b55737618f0ba87da875c
Glenn Fung <117829736+glenntfung@users.noreply.github.com>
2024-11-24 12:29:49 -0600

    Update config.py

--- Python Sample/config.py
+++ Python Sample/config.py
@@ -4,4 +4,4 @@ FPPAPI_KEY = ' ' ## Desensitization
 API_SECRET = ' ' ## Desensitization
 FACEPP_URL = 'https://api-us.faceplusplus.com/facepp/v3/detect' ## standard 
 
-GOOGLEAPI_KEY = "your_api_key_here"  # Replace with your API key
+GOOGLEAPI_KEY = " "  # Desensitization
